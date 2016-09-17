393445cca342531d428a53e887fc869639fe5fbf
Steve Fosdick <other@fosdick.me.uk>
2016-09-17 22:33:35 +0100

    Integrate VDFS into menu (on Linux)

--- src/vdfs.h
+++ src/vdfs.h
@@ -2,3 +2,7 @@ extern void vdfs_init();
 extern void vdfs_reset();
 extern void vdfs_close();
 extern void vdfs_write();
+
+extern int vdfs_enabled;
+extern const char *vdfs_get_root();
+extern void vdfs_set_root(const char *dir);
