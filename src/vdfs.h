3807ef3fab7ee359358a3e90a78414cd45445256
Steve Fosdick <other@fosdick.me.uk>
2016-09-19 01:52:26 +0100

    Enable VDFS ROM to run in ROM

--- src/vdfs.h
+++ src/vdfs.h
@@ -1,7 +1,8 @@
 extern void vdfs_init();
 extern void vdfs_reset();
 extern void vdfs_close();
-extern void vdfs_write();
+extern uint8_t vdfs_read(uint16_t addr);
+void vdfs_write(uint16_t addr, uint8_t value);
 
 extern int vdfs_enabled;
 extern const char *vdfs_get_root();
