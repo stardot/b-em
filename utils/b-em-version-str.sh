54efa7aacfcde4aed7a3b44631ade1b49decd36b
Thomas Adam <thomas@xteddy.org>
2017-02-15 23:17:54 +0000

    Ascertain version from git repo if !release
    When compiling b-em, if git is detected, use the latest git SHA1, otherwise
    use the released version (currently 2.2).
    
    Additionally:
    
    * Set the maintainer to Thomas' email address;
    * Use AM_SILENT_RULES

---
+++ utils/b-em-version-str.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# b-em-version-str: emits the version of b-em which is building.
+#		    If this is a release build, then the tag name is chomped
+#		    to remove extraneous git information.
+#
+#		    If it's a developer build, it's left as-is.
+#
+#
+#
+# Intended to be called from configure.ac (via autogen.sh)
+B_EM_VERSION=2.2
+
+
+if grep -q -i '^ISRELEASED="yes"' ./configure.ac; then
+	# A release build.  Strip the git information off the tag name.
+	git describe --tags --abbrev=0 2>/dev/null || echo "$B_EM_VERSION"
+else
+	git describe --always --long --dirty
+fi
