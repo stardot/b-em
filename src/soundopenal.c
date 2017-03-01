a386a9e8afcfda3f2a82c267deabab4b29833b28
David Banks <dave@hoglet.com>
2017-03-01 10:54:15 +0000

    Reworked soundopenal to avoid same code pasted three times

--- src/soundopenal.c
+++ src/soundopenal.c
@@ -8,19 +8,39 @@
 #include "sound.h"
 #include "soundopenal.h"
 
-static ALuint buffers[4]; // front and back buffers
-static ALuint source[3];     // audio source
-static ALuint buffersdd[4]; // front and back buffers
-static ALuint buffersm5[4]; // front and back buffers
-static ALenum format;     // internal format
+// Number of buffers per sources; openAL uses these cyclically
+#define NUM_AL_BUFFERS 4
 
-#define FREQ 31250
-#define FREQDD 44100
-#define FREQM5 46875
+#define NUM_AL_SOURCES 3
 
-#define BUFLEN (2000<<2)
-#define BUFLENDD (4410<<2)
-#define BUFLENM5 (3000<<2)
+#define SOURCE_SO 0 // normal sound
+#define SOURCE_DD 1 // disc drive noise
+#define SOURCE_M5 2 // music 5000
+
+#define DATA_MONO   0
+#define DATA_STEREO 1
+
+static ALuint source[NUM_AL_SOURCES];     // audio source
+
+static int source_freq[NUM_AL_SOURCES] = {
+       31250, // normal sound
+       44100, // disc drive noise
+       46875  // music 5000
+};
+
+// lengths in bytes (buffer holds stereo int16_t samples)
+
+#define MAXBUFLEN (4410<<2)
+
+static int source_buflen[NUM_AL_SOURCES] = {
+       2000<<2, // normal sound
+       4410<<2, // disc drive noise
+       3000<<2  // music 5000
+};
+
+static ALuint buffers[NUM_AL_SOURCES][NUM_AL_BUFFERS];
+
+static int16_t tempbuf[MAXBUFLEN>>1];
 
 static void check()
 {
@@ -50,98 +70,58 @@ void al_close()
         alutExit();
 }
 
-static int16_t tempbuf[BUFLEN>>1];
-static int16_t tempbufdd[BUFLENDD>>1];
-static int16_t tempbufm5[BUFLENM5>>1];
-
 void al_init()
 {
         int c;
-        format = AL_FORMAT_STEREO16;
+        int i;
+
         check();
 
-        alGenBuffers(4, buffers);
+        alGenSources(NUM_AL_SOURCES, source);
         check();
 
-        alGenSources(3, source);
-        check();
+        for (i = 0; i < NUM_AL_SOURCES; i++) {
 
-        alSource3f(source[0], AL_POSITION,        0.0, 0.0, 0.0);
-        alSource3f(source[0], AL_VELOCITY,        0.0, 0.0, 0.0);
-        alSource3f(source[0], AL_DIRECTION,       0.0, 0.0, 0.0);
-        alSourcef (source[0], AL_ROLLOFF_FACTOR,  0.0          );
-        alSourcei (source[0], AL_SOURCE_RELATIVE, AL_TRUE      );
-        check();
+                alGenBuffers(NUM_AL_BUFFERS, buffers[i]);
+                check();
 
-        memset(tempbuf, 0, BUFLEN);
+                alSource3f(source[i], AL_POSITION,        0.0, 0.0, 0.0);
+                alSource3f(source[i], AL_VELOCITY,        0.0, 0.0, 0.0);
+                alSource3f(source[i], AL_DIRECTION,       0.0, 0.0, 0.0);
+                alSourcef (source[i], AL_ROLLOFF_FACTOR,  0.0          );
+                alSourcei (source[i], AL_SOURCE_RELATIVE, AL_TRUE      );
+                check();
 
-        for (c = 0; c < 4; c++)
-            alBufferData(buffers[c], AL_FORMAT_STEREO16, tempbuf, BUFLEN, FREQ);
-        alSourceQueueBuffers(source[0], 4, buffers);
-        check();
-        alSourcePlay(source[0]);
-        check();
-//        printf("InitAL\n");
+                memset(tempbuf, 0, sizeof(tempbuf));
+                for (c = 0; c < NUM_AL_BUFFERS; c++) {
+                        alBufferData(buffers[i][c], AL_FORMAT_STEREO16, tempbuf, source_buflen[i], source_freq[i]);
+                }
 
-        alGenBuffers(4, buffersdd);
-        check();
+                alSourceQueueBuffers(source[i], NUM_AL_BUFFERS, buffers[i]);
+                check();
 
-        alSource3f(source[1], AL_POSITION,        0.0, 0.0, 0.0);
-        alSource3f(source[1], AL_VELOCITY,        0.0, 0.0, 0.0);
-        alSource3f(source[1], AL_DIRECTION,       0.0, 0.0, 0.0);
-        alSourcef (source[1], AL_ROLLOFF_FACTOR,  0.0          );
-        alSourcei (source[1], AL_SOURCE_RELATIVE, AL_TRUE      );
-        check();
-
-        memset(tempbufdd, 0, BUFLENDD);
-
-        for (c = 0; c < 4; c++)
-            alBufferData(buffersdd[c], AL_FORMAT_STEREO16, tempbufdd, BUFLENDD, FREQDD);
-        alSourceQueueBuffers(source[1], 4, buffersdd);
-        check();
-        alSourcePlay(source[1]);
-        check();
-
-        alGenBuffers(4, buffersm5);
-        check();
-
-        alSource3f(source[2], AL_POSITION,        0.0, 0.0, 0.0);
-        alSource3f(source[2], AL_VELOCITY,        0.0, 0.0, 0.0);
-        alSource3f(source[2], AL_DIRECTION,       0.0, 0.0, 0.0);
-        alSourcef (source[2], AL_ROLLOFF_FACTOR,  0.0          );
-        alSourcei (source[2], AL_SOURCE_RELATIVE, AL_TRUE      );
-        check();
-
-        memset(tempbufm5, 0, BUFLENM5);
-
-        for (c = 0; c < 4; c++)
-            alBufferData(buffersm5[c], AL_FORMAT_STEREO16, tempbufm5, BUFLENM5, FREQM5);
-        alSourceQueueBuffers(source[2], 4, buffersm5);
-        check();
-        alSourcePlay(source[2]);
-        check();
+                alSourcePlay(source[i]);
+                check();
+        }
 
 //        printf("InitAL\n");
 }
 
-static int16_t zbuf[16384];
-
-void al_givebuffer(int16_t *buf)
+static void al_givebuffer_generic(int16_t *buf, int i, int type)
 {
         int processed;
         int state;
         int c;
 
-//return;
-        alGetSourcei(source[0], AL_SOURCE_STATE, &state);
+        alGetSourcei(source[i], AL_SOURCE_STATE, &state);
 
         if (state == 0x1014)
         {
-                alSourcePlay(source[0]);
+                alSourcePlay(source[i]);
 //                printf("Resetting sound\n");
         }
 //        printf("State - %i %08X\n",state,state);
-        alGetSourcei(source[0], AL_BUFFERS_PROCESSED, &processed);
+        alGetSourcei(source[i], AL_BUFFERS_PROCESSED, &processed);
 
 //        printf("P ");
         check();
@@ -151,101 +131,38 @@ void al_givebuffer(int16_t *buf)
         {
                 ALuint buffer;
 
-                alSourceUnqueueBuffers(source[0], 1, &buffer);
+                alSourceUnqueueBuffers(source[i], 1, &buffer);
 //                printf("U ");
                 check();
 
-                for (c = 0; c < (BUFLEN >> 1); c++) zbuf[c] = buf[c >> 1];
+                if (type == DATA_MONO) {
+                   // Expand mono data to fill both channels
+                       for (c = 0; c < (source_buflen[i] >> 1); c++) tempbuf[c] = buf[c >> 1];
+                       buf = tempbuf;
+                }
 
-                alBufferData(buffer, AL_FORMAT_STEREO16, zbuf, BUFLEN, FREQ);
+                alBufferData(buffer, AL_FORMAT_STEREO16, buf, source_buflen[i], source_freq[i]);
 //                printf("B ");
                 check();
 
-                alSourceQueueBuffers(source[0], 1, &buffer);
+                alSourceQueueBuffers(source[i], 1, &buffer);
 //                printf("Q ");
                 check();
 
         }
 }
 
+void al_givebuffer(int16_t *buf)
+{
+        al_givebuffer_generic(buf, SOURCE_SO, DATA_MONO);
+}
+
 void al_givebufferdd(int16_t *buf)
 {
-        int processed;
-        int state;
-        int c;
-
-        if (!sound_ddnoise && !sound_tape) return;
-
-        alGetSourcei(source[1], AL_SOURCE_STATE, &state);
-
-        if (state == 0x1014)
-        {
-                alSourcePlay(source[1]);
-//                printf("Resetting sounddd\n");
-        }
-        alGetSourcei(source[1], AL_BUFFERS_PROCESSED, &processed);
-//bem_debug("Get source\n");
-        check();
-//bem_debug("Got source\n");
-        if (processed>=1)
-        {
-                ALuint buffer;
-
-//bem_debug("Unqueue\n");
-                alSourceUnqueueBuffers(source[1], 1, &buffer);
-                check();
-
-                for (c = 0; c < (BUFLENDD >> 1); c++) zbuf[c] = buf[c >> 1];//^0x8000;
-
-//bem_debug("BufferData\n");
-                alBufferData(buffer, AL_FORMAT_STEREO16, zbuf, BUFLENDD, FREQDD);
-                check();
-
-//bem_debug("Queue\n");
-                alSourceQueueBuffers(source[1], 1, &buffer);
-                check();
-        }
-
-//        bem_debug("DDnoise3\n");
+        al_givebuffer_generic(buf, SOURCE_DD, DATA_MONO);
 }
 
 void al_givebufferm5(int16_t *buf)
 {
-        int processed;
-        int state;
-        int c;
-
-        if (!sound_music5000) return;
-
-        alGetSourcei(source[2], AL_SOURCE_STATE, &state);
-
-        if (state == 0x1014)
-        {
-                alSourcePlay(source[2]);
-//                printf("Resetting soundm5\n");
-        }
-        alGetSourcei(source[2], AL_BUFFERS_PROCESSED, &processed);
-//bem_debug("Get source\n");
-        check();
-//bem_debug("Got source\n");
-        if (processed>=1)
-        {
-                ALuint buffer;
-
-//bem_debug("Unqueue\n");
-                alSourceUnqueueBuffers(source[2], 1, &buffer);
-                check();
-
-//                for (c = 0; c < (BUFLENM5 >> 1); c++) zbuf[c] = buf[c >> 1];//^0x8000;
-
-//bem_debug("BufferData\n");
-                alBufferData(buffer, AL_FORMAT_STEREO16, buf, BUFLENM5, FREQM5);
-                check();
-
-//bem_debug("Queue\n");
-                alSourceQueueBuffers(source[2], 1, &buffer);
-                check();
-        }
-
-//        bem_debug("DDnoise3\n");
+        al_givebuffer_generic(buf, SOURCE_M5, DATA_STEREO);
 }
