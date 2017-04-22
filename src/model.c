5671b19bb65b2698b13d55a70b0d73f0d19067f0
Steve Fosdick <steve@fosdick.me.uk>
2017-04-22 18:18:55 +0100

    models: avoid a  superfluous warning message due to -1 meaning "no tube"

--- src/model.c
+++ src/model.c
@@ -66,7 +66,7 @@ void model_check(void) {
     }
     if (models[curmodel].tube != -1)
         curtube = models[curmodel].tube;
-    if (curtube < 0 || curtube >= NUM_TUBES) {
+    if (curtube < -1 || curtube >= NUM_TUBES) {
         log_warn("No tube #%d, running with no tube instead", curtube);
         curtube = -1;
     }
