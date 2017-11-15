ae2a47f42828da944c8004ea54bc3c8b447ebe41
Steve Fosdick <steve@fosdick.me.uk>
2017-11-15 20:15:14 +0000

    vdfs: prepare ROM for *CDIR and improve help

--- src/vdfs.s
+++ src/vdfs.s
@@ -210,13 +210,13 @@ EQUS "Filing system selection:":EQUB 13
 EQUS "  DISK, DISC, ADFS, FADFS :":EQUB 10
 EQUS "Select VDFS if claimed":EQUB 13
 EQUS "  VDFS : Select VDFS":EQUB 13
-EQUS "  FSCLAIM ON : Claim DISC, ADFS":EQUB 13
-EQUS "  FSCLAIM OFF : Release DISC, ADFS":EQUB 13
+EQUS "  FSCLAIM ON : Claim DISC, ADFS, FADFS":EQUB 13
+EQUS "  FSCLAIM OFF : Release DISC, ADFS, FADFS":EQUB 13
 EQUS "  OSW7F (NONE)(<ver>) :":EQUB 10
 EQUS "Emulate Osword &7F memory corruption":EQUB 13
 EQUB 13
 EQUS "Utility commands:":EQUB 13
-EQUS "  QUIT or DESKTOP : return to RISC OS":EQUB 13
+EQUS "  QUIT or DESKTOP : terminate emulator":EQUB 13
 EQUS "  PAGE : force PAGE location":EQUB 13
 EQUS "  SHADOW : dummy command":EQUB 13
 EQUB 13
@@ -231,13 +231,15 @@ EQUS "  SRWRITE <start> <+len> <swadd> (<r#>)":EQUB 13
 EQUB 13
 EQUS "VDFS commands:":EQUB 13
 EQUS "  BACK : return to previous directory":EQUB 13
+EQUS "  DELETE : delete file or empty dir":EQUB 13
+EQUS "  CDIR : create a new directory":EQUB 13
 EQUS "  DIR : change current directory":EQUB 13
 EQUS "  LIB : change current library":EQUB 13
 EQUS "  INFO : show info on single file":EQUB 13
 EQUS "  EX : show info on all files in CSD":EQUB 13
 EQUS "  ACCESS, BACKUP, COMPACT, COPY,":EQUB 10
-EQUS "DESTROY, DRIVE, ENABLE, FORM, FREE,":EQUB 13
-EQUS "  MAP, MOUNT, RENAME, TITLE, VERIFY, ":EQUB 10
+EQUS "DESTROY, DRIVE, ENABLE, FORM,":EQUB 13
+EQUS "  FREE, MAP, MOUNT, TITLE, VERIFY,":EQUB 10
 EQUS "WIPE : trapped and ignored":EQUB 13
 EQUB 0
 \ --------------------
@@ -462,6 +464,7 @@ RTS                         :\ And return
 EQUS "ACCESS" :EQUB 13:EQUW access
 EQUS "BACK"   :EQUB 13:EQUW back
 EQUS "BACKUP" :EQUB 13:EQUW backup
+EQUS "CDIR"   :EQUB 13:EQUW cdir
 EQUS "COMPACT":EQUB 13:EQUW compact
 EQUS "COPY"   :EQUB 13:EQUW copy
 EQUS "DELETE" :EQUB 13:EQUW delete
@@ -491,6 +494,7 @@ TYA:CLC:ADC &F2:TAX:LDA &F3:ADC #0:TAY:RTS
 .access   :RTS
 .back     :LDA #&D5:STA PORT_CMD:RTS :\ Pass to host and return
 .backup   :RTS
+.cdir     :LDA #&DA:STA PORT_CMD:RTS :\ Pass to host and return
 .compact  :RTS
 .copy     :RTS
 .delete   :JSR F2toXY:STX &B0:STY &B1:LDX #&B0:LDY #&00:LDA #&06:JMP OSFILE
