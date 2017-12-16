46d39c9f0933245ac23a3daafe552873d5b7ec0f
Steve Fosdick <steve@fosdick.me.uk>
2017-12-16 23:23:34 +0000

    m2000: use symbolic constants in new generalised ACIA

--- src/acia.c
+++ src/acia.c
@@ -6,18 +6,26 @@
 #include "6502.h"
 #include "acia.h"
 
-#define DCD     4
-#define RECIEVE 1
+/* Status register flags */
+
+#define RXD_REG_FUL 0x01
+#define TXD_REG_EMP 0x02
+#define DCD         0x04
+#define CTS         0x08
+#define FRAME_ERR   0x10
+#define RX_OVERRUN  0x20
+#define PARITY_ERR  0x40
+#define INTERUPT    0x80
 
 void acia_updateint(ACIA *acia) {
-    if (((acia->status_reg & 0x80) && (acia->control_reg & 0x80)) || (!(acia->status_reg & 0x02) && ((acia->control_reg & 0x60) == 0x20)))
+    if (((acia->status_reg & INTERUPT) && (acia->control_reg & INTERUPT)) || (!(acia->status_reg & TXD_REG_EMP) && ((acia->control_reg & (PARITY_ERR|RX_OVERRUN)) == RX_OVERRUN)))
        interrupt|=4;
     else
        interrupt&=~4;
 }
 
 void acia_reset(ACIA *acia) {
-    acia->status_reg = (acia->status_reg & 8) | 6;
+    acia->status_reg = (acia->status_reg & CTS) | DCD | TXD_REG_EMP;
     acia_updateint(acia);
 }
 
@@ -26,12 +34,12 @@ uint8_t acia_read(ACIA *acia, uint16_t addr) {
 
     if (addr & 1) {
         temp = acia->rx_data_reg;
-        acia->status_reg &= ~0x81;
+        acia->status_reg &= ~(INTERUPT | RXD_REG_FUL);
         acia_updateint(acia);
         return temp;
     }
     else
-        return (acia->status_reg & 0x7F) | (acia->status_reg & acia->control_reg & 0x80);
+        return (acia->status_reg & ~INTERUPT) | (acia->status_reg & acia->control_reg & INTERUPT);
 }
 
 void acia_write(ACIA *acia, uint16_t addr, uint8_t val) {
@@ -54,7 +62,7 @@ void acia_write(ACIA *acia, uint16_t addr, uint8_t val) {
 void acia_dcdhigh(ACIA *acia) {
     if (acia->status_reg & DCD)
         return;
-    acia->status_reg |= DCD | 0x80;
+    acia->status_reg |= DCD | INTERUPT;
     acia_updateint(acia);
 }
 
@@ -65,7 +73,7 @@ void acia_dcdlow(ACIA *acia) {
 
 void acia_receive(ACIA *acia, uint8_t val) { /*Called when the acia recives some data*/
     acia->rx_data_reg = val;
-    acia->status_reg |= RECIEVE | 0x80;
+    acia->status_reg |= RXD_REG_FUL | INTERUPT;
     if (acia->rx_hook)
         acia->rx_hook(acia, val);
     acia_updateint(acia);
