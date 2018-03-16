7668b164dea3eb96565162f37100459cc731aec8
Steve Fosdick <steve@fosdick.me.uk>
2018-03-16 20:14:03 +0000

    allegro5: use the new Allegro memory viewer for Windows too.

--- src/debugger.c
+++ src/debugger.c
@@ -32,259 +32,6 @@ static void close_trace()
     }
 }
 
-#ifdef WIN32
-#include <windows.h>
-#include <wingdi.h>
-#include <process.h>
-
-static int debug_cons = 0;
-static HANDLE cinf, consf;
-
-static inline void debug_in(char *buf, size_t bufsize)
-{
-    int c;
-    DWORD len;
-
-    if ((c = ReadConsole(cinf, buf, bufsize, &len, NULL))) {
-        buf[len] = 0;
-        log_debug("debugger: read console, len=%d, s=%s", (int)len, buf);
-    } else
-        log_error("debugger: unable to read from console: %lu", GetLastError());
-}
-
-static void debug_out(const char *s, size_t len)
-{
-    WriteConsole(consf, s, len, NULL, NULL);
-}
-
-static void debug_outf(const char *fmt, ...)
-{
-    va_list ap;
-    char s[256];
-    size_t len;
-
-    va_start(ap, fmt);
-    len = vsnprintf(s, sizeof s, fmt, ap);
-    va_end(ap);
-    WriteConsole(consf, s, len, NULL, NULL);
-}
-
-static HANDLE debugthread;
-static HWND dhwnd;
-static int debugstarted = 0;
-static char DebugszClassName[] = "B-emDebugWnd";
-LRESULT CALLBACK DebugWindowProcedure (HWND, UINT, WPARAM, LPARAM);
-static HINSTANCE hinst;
-static uint8_t *usdat;
-
-void _debugthread(PVOID pvoid)
-{
-    MSG messages = {0};     /* Here messages to the application are saved */
-    WNDCLASSEX wincl;        /* Data structure for the windowclass */
-
-    HDC hDC;
-    HDC memDC;
-    HBITMAP memBM;
-    BITMAPINFO lpbmi;
-    int x;
-    int c,d;
-
-    usdat = malloc(256 * 256 * 4);
-    if (!debugstarted)
-    {
-        wincl.hInstance = hinst;
-        wincl.lpszClassName = DebugszClassName;
-        wincl.lpfnWndProc = DebugWindowProcedure;      /* This function is called by windows */
-        wincl.style = CS_DBLCLKS;                 /* Catch double-clicks */
-        wincl.cbSize = sizeof (WNDCLASSEX);
-
-        /* Use default icon and mouse-pointer */
-        wincl.hIcon = LoadIcon(hinst, "allegro_icon");
-        wincl.hIconSm = LoadIcon(hinst, "allegro_icon");
-        wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
-        wincl.lpszMenuName = NULL;                 /* No menu */
-        wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
-        wincl.cbWndExtra = 0;                      /* structure or the window instance */
-        /* Use Windows's default color as the background of the window */
-        wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
-
-        /* Register the window class, and if it fails quit the program */
-        if (!RegisterClassEx(&wincl))
-        {
-            printf("Registerclass failed\n");
-            return;
-        }
-    }
-    dhwnd = CreateWindowEx (
-        0,                   /* Extended possibilites for variation */
-        DebugszClassName,         /* Classname */
-        "Memory viewer",       /* Title Text */
-        WS_OVERLAPPEDWINDOW, /* default window */
-        CW_USEDEFAULT,       /* Windows decides the position */
-        CW_USEDEFAULT,       /* where the window ends up on the screen */
-        256 + (GetSystemMetrics(SM_CXFIXEDFRAME) * 2),/* The programs width */
-        256 + (GetSystemMetrics(SM_CYFIXEDFRAME) * 2) + GetSystemMetrics(SM_CYCAPTION) + 2,/* and height in pixels */
-        HWND_DESKTOP,        /* The window is a child-window to desktop */
-        NULL, /* No menu */
-        hinst,       /* Program Instance handler */
-        NULL                 /* No Window Creation data */
-    );
-    printf("Window create %08X\n", (uint32_t)dhwnd);
-    ShowWindow (dhwnd, SW_SHOWNORMAL);
-
-    hDC = GetDC(dhwnd);
-
-    memDC = CreateCompatibleDC ( hDC );
-    if (!memDC) printf("memDC failed!\n");
-        memBM = CreateCompatibleBitmap ( hDC, 256, 256 );
-    if (!memBM) printf("memBM failed!\n");
-        SelectObject ( memDC, memBM );
-
-    lpbmi.bmiHeader.biSize         = sizeof(BITMAPINFOHEADER);
-    lpbmi.bmiHeader.biWidth        = 256;
-    lpbmi.bmiHeader.biHeight       = -256;
-    lpbmi.bmiHeader.biPlanes       = 1;
-    lpbmi.bmiHeader.biBitCount     = 32;
-    lpbmi.bmiHeader.biCompression  = BI_RGB;
-    lpbmi.bmiHeader.biSizeImage    = 256 * 256 * 4;
-    lpbmi.bmiHeader.biClrUsed      = 0;
-    lpbmi.bmiHeader.biClrImportant = 0;
-
-    debugstarted = 1;
-    while (debug_core)
-    {
-        Sleep(20);
-        c += 16;
-        d = 0;
-        for (x = 0; x < 65536; x++)
-        {
-            usdat[d++] = fetchc[x] * 8;
-            usdat[d++] = readc[x]  * 8;
-            usdat[d++] = writec[x] * 8;
-            usdat[d++] = 0;
-            if (fetchc[x]) fetchc[x]--;
-            if (readc[x])  readc[x]--;
-            if (writec[x]) writec[x]--;
-        }
-
-        SetDIBitsToDevice(memDC, 0, 0, 256, 256, 0, 0, 0, 256, usdat, &lpbmi, DIB_RGB_COLORS);
-        BitBlt(hDC, 0, 0, 256, 256, memDC, 0, 0, SRCCOPY);
-
-        if (PeekMessage(&messages, NULL, 0, 0, PM_REMOVE))
-        {
-            /* Translate virtual-key messages into character messages */
-            TranslateMessage(&messages);
-            /* Send message to WindowProcedure */
-            DispatchMessage(&messages);
-        }
-        // if (indebug) pollmainwindow();
-    }
-    free(usdat);
-}
-
-BOOL CtrlHandler(DWORD fdwCtrlType)
-{
-    setquit();
-    return TRUE;
-}
-
-/*void _debugconsolethread(PVOID pvoid)
-{
-    int c,d;
-    return;
-    while (debug)
-    {
-        if (!gotstr)
-        {
-            c=ReadConsoleA(cinf,debugconsoleins,255,(LPDWORD)&d,NULL);
-            debugconsoleins[d]=0;
-            gotstr=1;
-        }
-        else
-            Sleep(10);
-    }
-}*/
-
-static void debug_cons_open(void)
-{
-    int c;
-
-    if (debug_cons++ == 0)
-    {
-        hinst = GetModuleHandle(NULL);
-        debugthread = (HANDLE)_beginthread(_debugthread, 0, NULL);
-        // debugconsolethread=(HANDLE)_beginthread(_debugconsolethread,0,NULL);
-
-        if ((c = AllocConsole())) {
-            SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE);
-            consf = GetStdHandle(STD_OUTPUT_HANDLE);
-            cinf  = GetStdHandle(STD_INPUT_HANDLE);
-        } else {
-            log_fatal("debugger: unable to allocate console: %lu", GetLastError());
-            exit(1);
-        }
-    }
-}
-
-static void debug_cons_close(void)
-{
-    if (--debug_cons == 0)
-    {
-        TerminateThread(debugthread, 0);
-        FreeConsole();
-    }
-}
-
-void debug_kill()
-{
-    close_trace();
-    debug_cons_close();
-    if (usdat) free(usdat);
-}
-
-LRESULT CALLBACK DebugWindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)                  /* handle the messages */
-    {
-        case WM_CREATE:
-            return 0;
-        case WM_DESTROY:
-            PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
-            break;
-        default:                       /* for messages that we don't deal with */
-            return DefWindowProc (hwnd, message, wParam, lParam);
-    }
-    return 0;
-}
-
-#else
-
-static inline void debug_in(char *buf, size_t bufsize)
-{
-    fgets(buf, bufsize, stdin);
-}
-
-static void debug_out(const char *s, size_t len)
-{
-    fwrite(s, len, 1, stdout);
-    fflush(stdout);
-}
-
-static void debug_outf(const char *fmt, ...)
-{
-    va_list ap;
-
-    va_start(ap, fmt);
-    vprintf(fmt, ap);
-    va_end(ap);
-    fflush(stdout);
-}
-
-void debug_kill()
-{
-    close_trace();
-}
-
 static ALLEGRO_THREAD  *mem_thread;
 
 static void *mem_thread_proc(ALLEGRO_THREAD *thread, void *data)
@@ -336,20 +83,129 @@ static void *mem_thread_proc(ALLEGRO_THREAD *thread, void *data)
     return NULL;
 }
 
-static void debug_cons_open(void)
+static void debug_memview_open(void)
 {
-    if ((mem_thread = al_create_thread(mem_thread_proc, NULL))) {
-        log_debug("debugger: memory view thread created");
-        al_start_thread(mem_thread);
+    if (!mem_thread) {
+        if ((mem_thread = al_create_thread(mem_thread_proc, NULL))) {
+            log_debug("debugger: memory view thread created");
+            al_start_thread(mem_thread);
+        }
+        else
+            log_error("debugger: failed to create memory view thread");
     }
-    else
-        log_error("debugger: failed to create memory view thread");
 }
 
-static inline void debug_cons_close(void)
+static void debug_memview_close(void)
 {
-    if (mem_thread)
+    if (mem_thread) {
         al_join_thread(mem_thread, NULL);
+        mem_thread = NULL;
+    }
+}
+
+#ifdef WIN32
+#include <windows.h>
+#include <wingdi.h>
+#include <process.h>
+
+static int debug_cons = 0;
+static HANDLE cinf, consf;
+
+static inline void debug_in(char *buf, size_t bufsize)
+{
+    int c;
+    DWORD len;
+
+    if ((c = ReadConsole(cinf, buf, bufsize, &len, NULL))) {
+        buf[len] = 0;
+        log_debug("debugger: read console, len=%d, s=%s", (int)len, buf);
+    } else
+        log_error("debugger: unable to read from console: %lu", GetLastError());
+}
+
+static void debug_out(const char *s, size_t len)
+{
+    WriteConsole(consf, s, len, NULL, NULL);
+}
+
+static void debug_outf(const char *fmt, ...)
+{
+    va_list ap;
+    char s[256];
+    size_t len;
+
+    va_start(ap, fmt);
+    len = vsnprintf(s, sizeof s, fmt, ap);
+    va_end(ap);
+    WriteConsole(consf, s, len, NULL, NULL);
+}
+
+static HANDLE debugthread;
+static HWND dhwnd;
+static int debugstarted = 0;
+static char DebugszClassName[] = "B-emDebugWnd";
+LRESULT CALLBACK DebugWindowProcedure (HWND, UINT, WPARAM, LPARAM);
+static HINSTANCE hinst;
+static uint8_t *usdat;
+
+static void debug_open(void)
+{
+    int c;
+
+    debug_memview_open();
+
+    if (debug_cons++ == 0)
+    {
+        hinst = GetModuleHandle(NULL);
+        if ((c = AllocConsole())) {
+            SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE);
+            consf = GetStdHandle(STD_OUTPUT_HANDLE);
+            cinf  = GetStdHandle(STD_INPUT_HANDLE);
+        } else {
+            log_fatal("debugger: unable to allocate console: %lu", GetLastError());
+            exit(1);
+        }
+    }
+}
+
+static void debug_close(void)
+{
+    if (--debug_cons == 0)
+        FreeConsole();
+    debug_memview_close();
+}
+
+#else
+
+static inline void debug_in(char *buf, size_t bufsize)
+{
+    fgets(buf, bufsize, stdin);
+}
+
+static void debug_out(const char *s, size_t len)
+{
+    fwrite(s, len, 1, stdout);
+    fflush(stdout);
+}
+
+static void debug_outf(const char *fmt, ...)
+{
+    va_list ap;
+
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
+    fflush(stdout);
+}
+
+static inline void debug_open(void)
+{
+    debug_memview_open();
+}
+
+static inline void debug_close(void)
+{
+    debug_memview_close();
 }
 
 #endif
@@ -363,9 +219,15 @@ static inline void debug_cons_close(void)
 #include "sn76489.h"
 #include "model.h"
 
+void debug_kill()
+{
+    close_trace();
+    debug_close();
+}
+
 static void enable_core_debug(void)
 {
-    debug_cons_open();
+    debug_open();
     debug_step = 1;
     debug_core = 1;
     log_info("debugger: debugging of core 6502 enabled");
@@ -376,7 +238,7 @@ static void enable_tube_debug(void)
 {
     if (curtube != -1)
     {
-        debug_cons_open();
+        debug_open();
         debug_step = 1;
         debug_tube = 1;
         log_info("debugger: debugging of tube CPU enabled");
@@ -388,7 +250,7 @@ static void disable_core_debug(void)
 {
     core6502_cpu_debug.debug_enable(0);
     log_info("debugger: debugging of core 6502 disabled");
-    debug_cons_close();
+    debug_close();
     debug_core = 0;
 }
 
@@ -398,7 +260,7 @@ static void disable_tube_debug(void)
     {
         tubes[curtube].debug->debug_enable(0);
         log_info("debugger: debugging of tube CPU disabled");
-        debug_cons_close();
+        debug_close();
         debug_tube = 0;
     }
 }
