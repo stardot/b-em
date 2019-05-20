465d9d1f43971d27893b269e36ad10ce46504f40
Steven Flintham <sgf@lemma.co.uk>
2019-05-20 21:32:58 +0100

    Snapshot

--- src/led.c
+++ src/led.c
@@ -9,7 +9,8 @@ static ALLEGRO_FONT *font;
 static const char *leds[] = {
     "Drive 0",
     "Drive 1",
-    "VDFS"
+    "VDFS",
+    "cassette\nmotor",
 };
 
 void led_init()
@@ -34,16 +35,29 @@ static void draw_led(int i, bool b)
     const int led_x1 = x1 + (box_width - led_width) / 2;
     const int led_y1 = y1 + (led_region_height - led_height) / 2;
     const char *label = leds[i];
-    const int text_width = al_get_text_width(font, label);
-    const int text_height = al_get_font_ascent(font);
-    const int text_x1 = x1 + (box_width - text_width) / 2;
-    const int text_y1 = y1 + led_region_height + (text_region_height - text_height) / 2;
     al_set_target_bitmap(led);
     al_draw_filled_rectangle(x1, y1, x1 + box_width - 1, y1 + box_height - 1, al_map_rgb(64,64,64));
     ALLEGRO_COLOR color_red = al_map_rgb(b ? 255 : 0, 0, 0);
     al_draw_filled_rectangle(led_x1, led_y1, led_x1 + led_width, led_y1 + led_height, color_red);
-    //al_draw_text(font, al_map_rgb(255, 255, 255), x1 + box_width / 2, y1 + led_region_height + text_region_height / 2, ALLEGRO_ALIGN_CENTRE, label);
-    al_draw_text(font, al_map_rgb(255, 255, 255), x1 + box_width / 2, text_y1, ALLEGRO_ALIGN_CENTRE, label);
+    const char *label_newline = strchr(label, '\n');
+    if (!label_newline) {
+        const int text_width = al_get_text_width(font, label);
+        const int text_height = al_get_font_ascent(font);
+        const int text_y1 = y1 + led_region_height + (text_region_height - text_height) / 2;
+        //al_draw_text(font, al_map_rgb(255, 255, 255), x1 + box_width / 2, y1 + led_region_height + text_region_height / 2, ALLEGRO_ALIGN_CENTRE, label);
+        al_draw_text(font, al_map_rgb(255, 255, 255), x1 + box_width / 2, text_y1, ALLEGRO_ALIGN_CENTRE, label);
+    }
+    else {
+        char *label1 = malloc(label_newline - label + 1);
+        memcpy(label1, label, label_newline - label);
+        label1[label_newline - label] = '\0';
+        const char *label2 = label_newline + 1;
+        const int text_height = al_get_font_line_height(font);
+        const int line_space = 2;
+        const int text_y1 = y1 + led_region_height + (text_region_height - 2 * text_height - line_space) / 2;
+        al_draw_text(font, al_map_rgb(255, 255, 255), x1 + box_width / 2, text_y1, ALLEGRO_ALIGN_CENTRE, label1);
+        al_draw_text(font, al_map_rgb(255, 255, 255), x1 + box_width / 2, text_y1 + text_height + line_space, ALLEGRO_ALIGN_CENTRE, label2);
+    }
     //al_draw_line(1.0, 5.0, 160.0, 2.0, color_red, 1.0);
 }
 
@@ -51,6 +65,9 @@ void led_update(bool b)
 {
     al_clear_to_color(al_map_rgb(0, 0, 0));
     draw_led(0, b);
+    draw_led(1, b);
+    draw_led(2, b);
+    draw_led(3, b);
     // SFTODO!
 }
 
