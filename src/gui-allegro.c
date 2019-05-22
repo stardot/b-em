96b5a24d4485474f80422a3e5b47cf8830a4ab56
Steve Fosdick <steve@fosdick.me.uk>
2019-05-22 23:50:37 +0100

    gui: allow upper case file extentions on disc image files.
    Thanks to Paul V for the patch (via e-mail).

--- src/gui-allegro.c
+++ src/gui-allegro.c
@@ -936,7 +936,8 @@ static void change_ddnoise_dtype(ALLEGRO_EVENT *event)
     ddnoise_init();
 }
 
-static const char all_dext[] = "*.ssd;*.dsd;*.img;*.adf;*.ads;*.adm;*.adl;*.sdd;*.ddd;*.fdi";
+static const char all_dext[] = "*.ssd;*.dsd;*.img;*.adf;*.ads;*.adm;*.adl;*.sdd;*.ddd;*.fdi;"
+                               "*.SSD;*.DSD;*.IMG;*.ADF;*.ADS;*.ADM;*.ADL;*.SDD;*.DDD;*.FDI;";
 
 void gui_allegro_event(ALLEGRO_EVENT *event)
 {
