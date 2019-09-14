69cd7ff875259dea20a3f2d75b48bd177e8797e3
Steve Fosdick <steve@fosdick.me.uk>
2019-09-14 20:11:02 +0100

    pdp11: use a more realistic clock speed for the PDP11.

--- src/model.c
+++ src/model.c
@@ -64,7 +64,7 @@ TUBE tubes[NUM_TUBES]=
     {"32016",          tube_32016_init, n32016_reset,    &n32016_cpu_debug,    0x0000, "",                 8 },
     {"6502 External",  tube_6502_init,  tube_6502_reset, &tube6502_cpu_debug,  0x0800, "6502Tube",         3 },
     {"6809",           tube_6809_init,  mc6809nc_reset,  &mc6809nc_cpu_debug,  0x0800, "6809Tube",        16 },
-    {"PDP11",          tube_pdp11_init, copro_pdp11_rst, &pdp11_cpu_debug,     0x0800, "PDP11Tube",        8 }
+    {"PDP11",          tube_pdp11_init, copro_pdp11_rst, &pdp11_cpu_debug,     0x0800, "PDP11Tube",        2 }
 };
 
 static fdc_type_t model_find_fdc(const char *name, const char *model)
