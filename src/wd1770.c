stardot/b-em

commit 56c959acf46df08a26873bc37122ca6f4c2e90f9

imd: enable more than one status code to reported to FDC.

src/wd1770.c
@@ -14,11 +14,6 @@
 void wd1770_callback();
 void wd1770_data(uint8_t dat);
 void wd1770_spindown();
-void wd1770_finishread();
-void wd1770_notfound();
-void wd1770_datacrcerror();
-void wd1770_headercrcerror();
-void wd1770_writeprotect();
 int  wd1770_getdata(int last);
 
 struct
@@ -45,6 +40,22 @@ static const unsigned step_times[4] = { 6000, 12000, 20000, 30000 };
 
 static int bytenum;
 
+static void short_spindown(void)
+{
+    motorspin = 15000;
+    fdc_time = 0;
+}
+
+static void wd1770_finishio(unsigned flags)
+{
+    wd1770.status = 0x80 | flags;
+    nmi = nmi_on_completion[fdc_type - FDC_ACORN];
+    if (flags)
+        short_spindown();
+    else
+        fdc_time = 200;
+}
+
 void wd1770_reset()
 {
     nmi = 0;
@@ -56,11 +67,7 @@ void wd1770_reset()
         fdc_callback       = wd1770_callback;
         fdc_data           = wd1770_data;
         fdc_spindown       = wd1770_spindown;
-        fdc_finishread     = wd1770_finishread;
-        fdc_notfound       = wd1770_notfound;
-        fdc_datacrcerror   = wd1770_datacrcerror;
-        fdc_headercrcerror = wd1770_headercrcerror;
-        fdc_writeprotect   = wd1770_writeprotect;
+        fdc_finishio       = wd1770_finishio;
         fdc_getdata        = wd1770_getdata;
         motorspin = 45000;
         if (motoron)
@@ -90,12 +97,6 @@ void wd1770_spindown()
 void wd1770_setspindown()
 {
     motorspin = 45000;
-}
-
-static void short_spindown(void)
-{
-    motorspin = 15000;
-    fdc_time = 0;
 }
 
 #define track0 (wd1770.track ? 0 : 4)
@@ -494,36 +495,6 @@ void wd1770_data(uint8_t dat)
     }
 }
 
-void wd1770_finishread()
-{
-    log_debug("wd1770: data i/o finished");
-    fdc_time = 200;
-}
-
-void wd1770_notfound()
-{
-    log_debug("wd1770: not found");
-    nmi |= nmi_on_completion[fdc_type - FDC_ACORN];
-    wd1770.status = 0x90;
-    short_spindown();
-}
-
-void wd1770_datacrcerror()
-{
-    log_debug("wd1770: data CRC error");
-    nmi = nmi_on_completion[fdc_type - FDC_ACORN];
-    wd1770.status = 0x88;
-    short_spindown();
-}
-
-void wd1770_headercrcerror()
-{
-    log_debug("wd1770: header CRC error");
-    nmi = nmi_on_completion[fdc_type - FDC_ACORN];
-    wd1770.status = 0x98;
-    short_spindown();
-}
-
 int wd1770_getdata(int last)
 {
     //log_debug("wd1770: disc get data");
@@ -538,12 +509,4 @@ int wd1770_getdata(int last)
     }
     wd1770.written = 0;
     return wd1770.data;
-}
-
-void wd1770_writeprotect()
-{
-    fdc_time = 0;
-    nmi = nmi_on_completion[fdc_type - FDC_ACORN];
-    wd1770.status = 0xC0;
-    wd1770_setspindown();
 }

src/disc.c
@@ -27,11 +27,7 @@ int motoron;
 void (*fdc_callback)();
 void (*fdc_data)(uint8_t dat);
 void (*fdc_spindown)();
-void (*fdc_finishread)();
-void (*fdc_notfound)();
-void (*fdc_datacrcerror)();
-void (*fdc_headercrcerror)();
-void (*fdc_writeprotect)();
+void (*fdc_finishio)();
 int  (*fdc_getdata)(int last);
 
 void disc_load(int drive, ALLEGRO_PATH *fn)
@@ -88,7 +84,7 @@ void disc_poll()
         {
                 disc_notfound--;
                 if (!disc_notfound)
-                   fdc_notfound();
+                   fdc_finishio(FDC_NOT_FOUND);
         }
 }
 

src/disc.h
@@ -36,14 +36,26 @@ int disc_verify(int drive, int track, int density);
 
 extern int disc_time;
 
+/* FDC status/error indications.
+ *
+ * These flag values allow a disc format implementation to report one
+ * or more statuses back to the FDC emulation.  These bit masks here
+ * are as used by the WD1770.  With the exception of "Deleted Data",
+ * the i8271 does not allow more than one of these errors to be
+ * reported at the same time so the i8271 emulation will convert
+ * these flags into a suitable status value.
+ */
+#define FDC_SUCCESS        0x00
+#define FDC_HEADER_CRC_ERR 0x04
+#define FDC_DATA_CRC_ERR   0x08
+#define FDC_NOT_FOUND      0x10
+#define FDC_DELETED_DATA   0x20
+#define FDC_WRITE_PROTECT  0x40
+
 extern void (*fdc_callback)(void);
 extern void (*fdc_data)(uint8_t dat);
 extern void (*fdc_spindown)(void);
-extern void (*fdc_finishread)(void);
-extern void (*fdc_notfound)(void);
-extern void (*fdc_datacrcerror)(void);
-extern void (*fdc_headercrcerror)(void);
-extern void (*fdc_writeprotect)(void);
+extern void (*fdc_finishio)(unsigned flags);
 extern int  (*fdc_getdata)(int last);
 extern int fdc_time;
 

src/fdi.c
@@ -29,28 +29,28 @@ static uint16_t CRCTable[256];
 
 static void fdi_setupcrc(uint16_t poly, uint16_t rvalue)
 {
-	int c = 256, bc;
-	uint16_t crctemp;
+    int c = 256, bc;
+    uint16_t crctemp;
 
-	while(c--)
-	{
-		crctemp = c << 8;
-		bc = 8;
+    while(c--)
+    {
+        crctemp = c << 8;
+        bc = 8;
 
-		while(bc--)
-		{
-			if(crctemp & 0x8000)
-			{
-				crctemp = (crctemp << 1) ^ poly;
-			}
-			else
-			{
-				crctemp <<= 1;
-			}
-		}
+        while(bc--)
+        {
+            if(crctemp & 0x8000)
+            {
+                crctemp = (crctemp << 1) ^ poly;
+            }
+            else
+            {
+                crctemp <<= 1;
+            }
+        }
 
-		CRCTable[c] = crctemp;
-	}
+        CRCTable[c] = crctemp;
+    }
 }
 
 static void fdi_seek(int drive, int track)
@@ -167,7 +167,7 @@ static uint16_t crc;
 
 static void calccrc(uint8_t byte)
 {
-	crc = (crc << 8) ^ CRCTable[(crc >> 8)^byte];
+    crc = (crc << 8) ^ CRCTable[(crc >> 8)^byte];
 }
 
 static void fdi_poll()
@@ -185,7 +185,7 @@ static void fdi_poll()
         if (fdi_inwrite)
         {
                 fdi_inwrite=0;
-                fdc_writeprotect();
+                fdc_finishio(FDC_WRITE_PROTECT);
                 return;
         }
         if (!fdi_inread && !fdi_inreadaddr) return;
@@ -195,7 +195,7 @@ static void fdi_poll()
                 if (fdi_revs == 3)
                 {
 //                        printf("Not found!\n");
-                        fdc_notfound();
+                        fdc_finishio(FDC_NOT_FOUND);
                         fdi_inread = fdi_inreadaddr = 0;
                         return;
                 }
@@ -227,9 +227,9 @@ static void fdi_poll()
                                                         if (fdi_inreadaddr)
                                                         {
                                                                 fdc_data(fdi_sector);
-                                                                fdc_finishread();
+                                                                fdc_finishio(FDC_SUCCESS);
                                                         }
-                                                        else             fdc_headercrcerror();
+                                                        else             fdc_finishio(FDC_HEADER_CRC_ERR);
                                                         return;
                                                 }
                                                 if (fdi_sectordat[0] == fdi_track && fdi_sectordat[2] == fdi_sector && fdi_inread && !fdi_inreadaddr)
@@ -241,7 +241,7 @@ static void fdi_poll()
                                                 }
                                                 if (fdi_inreadaddr)
                                                 {
-                                                        fdc_finishread();
+                                                        fdc_finishio(FDC_SUCCESS);
                                                         fdi_inreadaddr = 0;
                                                 }
                                         }
@@ -263,14 +263,13 @@ static void fdi_poll()
 //                                                printf("Data CRC error : %02X %02X %02X %02X %i %04X %02X%02X %i\n",crc>>8,crc&0xFF,sectorcrc[0],sectorcrc[1],fdipos,crc,sectorcrc[0],sectorcrc[1],ftracklen[0][0][fdidensity]);
                                                 inreadop = 0;
                                                 fdc_data(decodefm(lastfdidat[1]));
-                                                fdc_finishread();
-                                                fdc_datacrcerror();
+                                                fdc_finishio(FDC_DATA_CRC_ERR);
                                                 readdatapoll = 0;
                                                 return;
                                         }
 //                                        printf("End of FDI read %02X %02X %02X %02X\n",crc>>8,crc&0xFF,sectorcrc[0],sectorcrc[1]);
                                         fdc_data(decodefm(lastfdidat[1]));
-                                        fdc_finishread();
+                                        fdc_finishio(FDC_SUCCESS);
                                 }
                                 else if (lastfdidat[1] != 0)
                                    fdc_data(decodefm(lastfdidat[1]));
@@ -365,10 +364,10 @@ void fdi_load(int drive, const char *fn)
         writeprot[drive] = fwriteprot[drive] = 1;
         fdi_f[drive] = fopen(fn, "rb");
         if (!fdi_f[drive])
-	{
-		log_warn("fdi: unable to open FDI disc image '%s': %s", fn, strerror(errno));
-		return;
-	}
+    {
+        log_warn("fdi: unable to open FDI disc image '%s': %s", fn, strerror(errno));
+        return;
+    }
         fdi_h[drive] = fdi2raw_header(fdi_f[drive]);
 //        if (!fdih[drive]) printf("Failed to load!\n");
         fdi_lasttrack[drive] = fdi2raw_get_last_track(fdi_h[drive]);

src/i8271.c
@@ -13,11 +13,6 @@
 static void i8271_callback();
 static void i8271_data(uint8_t dat);
 static void i8271_spindown();
-static void i8271_finishread();
-static void i8271_notfound();
-static void i8271_datacrcerror();
-static void i8271_headercrcerror();
-static void i8271_writeprotect();
 static int  i8271_getdata(int last);
 
 static int bytenum;
@@ -45,6 +40,42 @@ struct
         uint8_t drvout;
 } i8271;
 
+static void short_spindown(void)
+{
+    motorspin = 15000;
+    fdc_time = 0;
+}
+
+static void i8271_NMI(void)
+{
+    if (i8271.status & 8)
+        nmi = 1;
+    else
+        nmi = 0;
+}
+
+static void i8271_finishio(unsigned flags)
+{
+    unsigned result = 0;
+    if (flags & FDC_HEADER_CRC_ERR)
+        result = 0x0c;
+    else if (flags & FDC_DATA_CRC_ERR)
+        result = 0x0e;
+    else if (flags & FDC_NOT_FOUND)
+        result = 0x18;
+    else if (flags & FDC_WRITE_PROTECT)
+        result = 0x12;
+    if (!result) {
+        if (flags & FDC_DELETED_DATA)
+            result |= 0x20;
+        fdc_time = 200;
+    }
+    else
+        short_spindown();
+    i8271.result = result;
+    i8271_NMI();
+}
+
 void i8271_reset()
 {
         if (fdc_type == FDC_I8271)
@@ -52,11 +83,14 @@ void i8271_reset()
                 fdc_callback       = i8271_callback;
                 fdc_data           = i8271_data;
                 fdc_spindown       = i8271_spindown;
+                fdc_finishio       = i8271_finishio;
+                /*
                 fdc_finishread     = i8271_finishread;
                 fdc_notfound       = i8271_notfound;
                 fdc_datacrcerror   = i8271_datacrcerror;
                 fdc_headercrcerror = i8271_headercrcerror;
                 fdc_writeprotect   = i8271_writeprotect;
+                * */
                 fdc_getdata        = i8271_getdata;
                 motorspin = 45000;
         }
@@ -68,17 +102,6 @@ void i8271_reset()
         i8271.command = 0xFF;
         i8271.realtrack[0] = i8271.realtrack[1] = 0;
 }
-
-static void i8271_NMI()
-{
-        if (i8271.status & 8)
-        {
-                nmi = 1;
-//                printf("NMI\n");
-        }
-        else                nmi = 0;
-}
-
 
 void i8271_spinup()
 {
@@ -101,12 +124,6 @@ static void i8271_spindown()
 void i8271_setspindown()
 {
     motorspin = 45000;
-}
-
-static void short_spindown(void)
-{
-    motorspin = 15000;
-    fdc_time = 0;
 }
 
 int params[][2]=
@@ -470,38 +487,6 @@ static void i8271_data(uint8_t dat)
         bytenum++;
 }
 
-static void i8271_finishread()
-{
-        fdc_time = 200;
-}
-
-static void i8271_notfound()
-{
-        i8271.result = 0x18;
-        i8271.status = 0x18;
-        i8271_NMI();
-        short_spindown();
-//        printf("Not found 8271\n");
-}
-
-static void i8271_datacrcerror()
-{
-        i8271.result = 0x0E;
-        i8271.status = 0x18;
-        i8271_NMI();
-        short_spindown();
-//        printf("CRCdat 8271\n");
-}
-
-static void i8271_headercrcerror()
-{
-        i8271.result = 0x0C;
-        i8271.status = 0x18;
-        i8271_NMI();
-        short_spindown();
-//        printf("CRChead 8271\n");
-}
-
 static int i8271_getdata(int last)
 {
 //        printf("Disc get data %i\n",bytenum);
@@ -515,12 +500,4 @@ static int i8271_getdata(int last)
         }
         i8271.written = 0;
         return i8271.data;
-}
-
-static void i8271_writeprotect()
-{
-        i8271.result = 0x12;
-        i8271.status = 0x18;
-        i8271_NMI();
-        short_spindown();
 }

src/imd.c
@@ -566,7 +566,7 @@ static void imd_poll_writesect1(void)
                 struct imd_sect *new_sect = malloc(sizeof(struct imd_sect)+cur_sect->sectsize);
                 if (!new_sect) {
                     log_error("imd: out of memory reallocating sector");
-                    fdc_datacrcerror();
+                    fdc_finishio(FDC_DATA_CRC_ERR);
                     return;
                 }
                 /* Link the new sector into the list in place of the old */
@@ -597,13 +597,13 @@ static void imd_poll_writesect1(void)
             *data++ = b;
             state = ST_WRITESECTOR2;
             if (count == 0) {
-                fdc_finishread();
+                fdc_finishio(0);
                 state = ST_IDLE;
             }
         }
         else if (count == 0) {
             cur_sect->data[0] = cdata;
-            fdc_finishread();
+            fdc_finishio(0);
             state = ST_IDLE;
             cur_sect->mode = mode_compressed(cur_sect->mode);
         }
@@ -627,7 +627,7 @@ static void imd_poll_writesect2(void)
         log_debug("imd: imd_poll_writesect2 byte=%02X", c);
         *data++ = c;
         if (count == 0) {
-            fdc_finishread();
+            fdc_finishio(0);
             state = ST_IDLE;
         }
     }
@@ -706,7 +706,7 @@ static void imd_poll_format_sectsz(void)
     if (count)
         state = ST_FORMAT_CYLID;
     else {
-        fdc_finishread();
+        fdc_finishio(0);
         state = ST_IDLE;
         imd_dump(&imd_discs[0]);
     }
@@ -751,7 +751,7 @@ static void imd_poll_wrtrack_initial(void)
     if (b == 0xfe)
         state = ST_WRTRACK_CYLID;
     else if (--count == 0) {
-        fdc_finishread();
+        fdc_finishio(0);
         state = ST_IDLE;
         cur_trk = NULL;
     }
@@ -943,7 +943,7 @@ static void imd_poll(void)
 
         case ST_NOTFOUND:
             if (--count == 0) {
-                fdc_notfound();
+                fdc_finishio(FDC_NOT_FOUND);
                 state = ST_IDLE;
             }
             break;
@@ -951,7 +951,7 @@ static void imd_poll(void)
         case ST_READSECTOR:
             fdc_data(*data++);
             if (--count == 0) {
-                fdc_finishread();
+                fdc_finishio(0);
                 state = ST_IDLE;
             }
             break;
@@ -959,14 +959,14 @@ static void imd_poll(void)
         case ST_READCOMPR:
             fdc_data(cdata);
             if (--count == 0) {
-                fdc_finishread();
+                fdc_finishio(0);
                 state = ST_IDLE;
             }
             break;
 
         case ST_WRITEPROT:
             log_debug("imd: poll, write protected during write sector");
-            fdc_writeprotect();
+            fdc_finishio(FDC_WRITE_PROTECT);
             state = ST_IDLE;
             break;
 
@@ -1014,7 +1014,7 @@ static void imd_poll(void)
 
         case ST_READ_ADDR6:
             state = ST_IDLE;
-            fdc_finishread();
+            fdc_finishio(0);
             break;
 
         case ST_FORMAT_CYLID:

src/sdf-acc.c
@@ -252,7 +252,7 @@ static void sdf_poll()
 
         case ST_NOTFOUND:
             if (--count == 0) {
-                fdc_notfound();
+                fdc_finishio(FDC_NOT_FOUND);
                 state = ST_IDLE;
             }
             break;
@@ -262,7 +262,7 @@ static void sdf_poll()
                 c = 0xe5;
             fdc_data(c);
             if (--count == 0) {
-                fdc_finishread();
+                fdc_finishio(0);
                 state = ST_IDLE;
             }
             break;
@@ -270,7 +270,7 @@ static void sdf_poll()
         case ST_WRITESECTOR:
             if (writeprot[sdf_drive]) {
                 log_debug("sdf: poll, write protected during write sector");
-                fdc_writeprotect();
+                fdc_finishio(FDC_WRITE_PROTECT);
                 state = ST_IDLE;
                 break;
             }
@@ -281,7 +281,7 @@ static void sdf_poll()
             } else {
                 putc(c, sdf_fp[sdf_drive]);
                 if (count == 0) {
-                    fdc_finishread();
+                    fdc_finishio(0);
                     state = ST_IDLE;
                 }
             }
@@ -323,7 +323,7 @@ static void sdf_poll()
 
         case ST_READ_ADDR6:
             state = ST_IDLE;
-            fdc_finishread();
+            fdc_finishio(FDC_WRITE_PROTECT);
             sdf_sector++;
             if (sdf_sector == geometry[sdf_drive]->sectors_per_track)
                 sdf_sector = 0;
@@ -332,7 +332,7 @@ static void sdf_poll()
         case ST_FORMAT_CYLID:
             if (writeprot[sdf_drive]) {
                 log_debug("sdf: poll, write protected during format");
-                fdc_writeprotect();
+                fdc_finishio(FDC_WRITE_PROTECT);
                 state = ST_IDLE;
                 break;
             }
@@ -361,7 +361,7 @@ static void sdf_poll()
             }
             if (count == 0) {
                 state = ST_IDLE;
-                fdc_finishread();
+                fdc_finishio(0);
             }
             else
                 state = ST_FORMAT_CYLID;

commit a15ec826f9ca15d62c4feb16e9d4ca47d78e5a8d

wd1770: fix read corruption introduced in 56c959ac

src/wd1770.c
@@ -49,10 +49,12 @@ static void short_spindown(void)
 
 static void wd1770_finishio(unsigned flags)
 {
-    wd1770.status = 0x80 | flags;
-    nmi = nmi_on_completion[fdc_type - FDC_ACORN];
-    if (flags)
+    log_debug("wd1770: finishio, old_status=%02X, flags=%02X", wd1770.status, flags);
+    wd1770.status |= flags;
+    if (flags & (FDC_NOT_FOUND|FDC_WRITE_PROTECT)) {
         short_spindown();
+        nmi = nmi_on_completion[fdc_type - FDC_ACORN];
+    }
     else
         fdc_time = 200;
 }
@@ -213,6 +215,13 @@ static void wd1770_cmd(unsigned val)
             if (((val & 0xc) || (wd1770.command & 0xf0) == 0xb0) && nmi_on_completion[fdc_type - FDC_ACORN])
                 nmi = 1;
             wd1770_setspindown();
+            break;
+
+        case 0xE: /* read track */
+            log_debug("wd1770: read track side=%d track=%d dens=%d, ctrl=%d\n", wd1770.curside, wd1770.track, wd1770.density, wd1770.ctrl);
+            wd1770.status = 0x83;
+            nmi |= 2;
+            disc_readtrack(curdrive, wd1770.track, wd1770.curside, wd1770.density);
             break;
 
         case 0xF: /*Write track*/
@@ -440,7 +449,7 @@ void wd1770_callback()
         break;
 
     case 8: /*Read sector*/
-        wd1770.status = 0x80;
+        wd1770.status &= ~0x01;
         wd1770_setspindown();
         if (nmi_on_completion[fdc_type - FDC_ACORN])
             nmi |= 1;