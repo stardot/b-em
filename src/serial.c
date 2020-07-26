stardot/b-em

commit 21d63f1f563872108bd58c87337c2b7eb739bdf8

leds: avoid spurious reappearance of LEDS during cassette reading.
When reading data from cassette the OS briefly turns off the cassette
motor relay between blocks.  When LEDs were to set to "when changed" or
"when changed or transient" this caused the LED bar to re-appear then
fade out again.  Accoring to a discussion on the forums, the LED
momentarily going out is not percepible.

https://stardot.org.uk/forums/viewtopic.php?f=4&p=280528

This comment therefore adds a delay between a 0 being written to the bit
in the Serial ULA and turning of the B-Em cassette LED.  This avoids the
bar popping up when there is nothing of interest on it.

src/serial.c
@@ -27,10 +27,19 @@ void serial_write(uint16_t addr, uint8_t val)
         serial_reg = val;
         serial_transmit_rate = val & 0x7;
         serial_recive_rate = (val >> 3) & 0x7;
-        if (motor != (val & 0x80))
-            tapenoise_motorchange(val>>7);
-        led_update(LED_CASSETTE_MOTOR, val & 0x80, 0);
-        motor = (val & 0x80) && tape_loaded;
+        int new_motor = val & 0x80;
+        if (new_motor && !motor) {
+            log_debug("serial: cassette motor on");
+            tapenoise_motorchange(1);
+            led_update(LED_CASSETTE_MOTOR, 1, 0);
+            motor = tape_loaded;
+        }
+        else if (!new_motor) {
+            log_debug("serial: cassette motor off");
+            tapenoise_motorchange(0);
+            motor = 0;
+            tapeledcount = 2;
+        }
         if (val & 0x40)
         {
             /*RS423*/

src/main.c
@@ -483,6 +483,12 @@ static void main_timer(ALLEGRO_EVENT *event)
         if (ddnoise_ticks > 0 && --ddnoise_ticks == 0)
             ddnoise_headdown();
 
+        if (tapeledcount) {
+            if (--tapeledcount == 0 && !motor) {
+                log_debug("main: delayed cassette motor LED off");
+                led_update(LED_CASSETTE_MOTOR, 0, 0);
+            }
+        }
         if (led_ticks > 0 && --led_ticks == 0)
             led_timer_fired();
 

src/tape.c
@@ -1,11 +1,12 @@
 #include "b-em.h"
+#include "led.h"
 #include "tape.h"
 #include "serial.h"
 #include "tapenoise.h"
 #include "uef.h"
 #include "csw.h"
 
-int tapelcount,tapellatch;
+int tapelcount,tapellatch,tapeledcount;
 
 bool tape_loaded = false;
 bool fasttape = false;

src/tape.h
@@ -11,7 +11,7 @@ void tape_close(void);
 void tape_poll(void);
 void tape_receive(ACIA *acia, uint8_t data);
 
-extern int tapelcount,tapellatch;
+extern int tapelcount,tapellatch,tapeledcount;
 extern bool fasttape;
 
 #endif

commit 976ec2cf53227b7f708706f9eb43ca179b54b0e1

leds: avoid cassette relay click when motor already off.
This fixes a bug introduced in 21d63f1f563872108bd58c87337c2b7eb739bdf8
where, when the cassette motor was already off, writing a zero to the
relevant bit in the serial ULA would produce another click from the
relay.  This would happen on Break or with *MOTOR 0.

src/serial.c
@@ -34,7 +34,7 @@ void serial_write(uint16_t addr, uint8_t val)
             led_update(LED_CASSETTE_MOTOR, 1, 0);
             motor = tape_loaded;
         }
-        else if (!new_motor) {
+        else if (!new_motor && motor) {
             log_debug("serial: cassette motor off");
             tapenoise_motorchange(0);
             motor = 0;