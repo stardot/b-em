stardot/b-em

commit eb19a01e40945dd7b1f52f107cc3a75ce74f52f7

integra: fix failure to enter reset code on Ctrl-@-Break.
See https://stardot.org.uk/forums/viewtopic.php?f=4&t=17675&p=249181#p248995

There was an issue where Ctrl-@-Break would offer the option to reset
the configuration on real hardware but not in B-Em or in BeebEm.  This
turned out to be due to the failure to emulate the resetting of RTC
registers B and C.  This commit adds an implementation of this.

src/main.c
@@ -118,7 +118,7 @@ void main_reset()
     else               tube_exec = NULL;
     tube_reset();
 
-    memset(ram, 0, 64 * 1024);
+    memset(ram, 0, RAM_SIZE);
 }
 
 static const char helptext[] =
@@ -376,6 +376,7 @@ static void main_key_down(ALLEGRO_EVENT *event)
             wd1770_reset();
             sid_reset();
             music5000_reset();
+            cmos_reset();
 
             if (curtube != -1)
                 tubes[curtube].reset();

src/cmos.c
@@ -179,6 +179,7 @@ uint8_t cmos_read_data_integra(void)
             if (++uip_count == 100) {
                 val |= 0x80;
                 uip_count = 0;
+                log_debug("cmos: read_data_integra, faking update");
             }
             log_debug("cmos: read_data_integra, return register A %02X", cmos_data);
         }
@@ -186,6 +187,12 @@ uint8_t cmos_read_data_integra(void)
             log_debug("cmos: read_data_integra, return RAM data %02X", cmos_data);
     }
     return val;
+}
+
+void cmos_reset(void)
+{
+    cmos[0xb] &= 0x87; /* clear bits in register B */
+    cmos[0xc] = 0;
 }
 
 void cmos_load(MODEL m) {

src/cmos.h
@@ -7,6 +7,7 @@ void cmos_write_addr_integra(uint8_t val);
 void cmos_write_data_integra(uint8_t val);
 uint8_t cmos_read_data_integra(void);
 uint8_t cmos_read(void);
+void cmos_reset(void);
 void cmos_load(MODEL m);
 void cmos_save(MODEL m);
 

src/mem.c
@@ -27,7 +27,6 @@ void mem_init() {
     ram = (uint8_t *)malloc(RAM_SIZE);
     rom = (uint8_t *)malloc(ROM_NSLOT * ROM_SIZE);
     os  = (uint8_t *)malloc(ROM_SIZE);
-    memset(ram, 0, RAM_SIZE);
     os_dir  = al_create_path_for_directory("roms/os");
     rom_dir = al_create_path_for_directory("roms/general");
 }

commit 608eb15ad89f8003736f2b2dbc310759e9ab72c0

integra: fix Issue#210
This fixes the disappearance of the call to cmos_reset, introduced in
commit eb19a01 to resolve failure of the integra IBOS to prompt for a
reset when the key sequence was activated and which, by commit ec3fc56,
had disappeared.

src/main.c
@@ -408,6 +408,7 @@ void main_key_break(void)
     wd1770_reset();
     sid_reset();
     music5000_reset();
+    cmos_reset();
     paula_reset();
 
     if (curtube != -1)