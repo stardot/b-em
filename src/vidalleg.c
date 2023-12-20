abe5e4993571d51ee6eddead3864bf1c0f140957
Steve Fosdick <steve@fosdick.me.uk>
2023-12-20 23:34:12 +0000

    Issue214: select buffer 'b' for locking to unselect back buffer.

--- src/vidalleg.c
+++ src/vidalleg.c
@@ -408,6 +408,7 @@ static inline void blit_screen(void)
                 al_unlock_bitmap(b);
                 upscale_only(b, firstx, firsty << 1, xsize, ysize  << 1, scr_x_start, scr_y_start, scr_x_size, scr_y_size);
         }
+        al_set_target_bitmap(b);
         region = al_lock_bitmap(b, ALLEGRO_PIXEL_FORMAT_ARGB_8888, ALLEGRO_LOCK_WRITEONLY);
     }
 }
